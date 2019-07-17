ffffc1c7031ce9c0d21c6f00db8f147e48fac29a
ALau2088 <alau12004@yahoo.com>
2019-07-17 15:43:58 -0700

    initial commit

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,32 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var key = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[key] = value;
+    key += 1;
+    return storage[key];
   };
 
   someInstance.dequeue = function() {
+    if (key !== 0) {
+      var tempObj = {};
+      // Decrement key
+      // Store first element to be dequeue
+      var firstElement = storage[Object.keys(storage)[0]];
+      for (var i = 1; i < Object.keys(storage).length; i++) {
+        tempObj[Object.keys(storage)[i]] = storage[i];
+      }
+      storage = tempObj;
+      return firstElement;
+    }
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,36 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var key = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // Add value
+    storage[key] = value;
+    // increment key
+    key += 1;
+
   };
 
   someInstance.pop = function() {
+    if (key !== 0) {
+      var tempObj = {};
+      // Decrement key
+      key -= 1;
+      // Store last element to to popped
+      var lastElement = storage[key];
+
+      for (var i = 0; i < Object.keys(storage).length - 1; i++) {
+        tempObj[Object.keys(storage)[i]] = storage[i];
+      }
+      storage = tempObj;
+
+      return lastElement;
+    }
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
