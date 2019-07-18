214512e20052cbf8cbf3a5d0838af3c16339ecf8
ALau2088 <alau12004@yahoo.com>
2019-07-18 01:47:48 -0700

    complete es6 pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,33 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.key = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.key] = value;
+    this.key += 1;
+    return this.storage[this.key];
+  }
+
+  dequeue() {
+    if (this.key !== 0) {
+      var tempObj = {};
+      // Decrement key
+      // Store first element to be dequeue
+      var firstElement = this.storage[Object.keys(this.storage)[0]];
+      for (var i = 1; i < Object.keys(this.storage).length; i++) {
+        tempObj[Object.keys(this.storage)[i]] = this.storage[i];
+      }
+      this.storage = tempObj;
+      return firstElement;
+    }
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
+
+var aQueue = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,37 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.key = 0;
   }
 
+  push(value) {
+    // Add value
+    this.storage[this.key] = value;
+    // increment key
+    this.key += 1;
+  }
+
+  pop() {
+    if (this.key !== 0) {
+      var tempObj = {};
+      // Decrement key
+      this.key -= 1;
+      // Store last element to to popped
+      var lastElement = this.storage[this.key];
+
+      for (var i = 0; i < Object.keys(this.storage).length - 1; i++) {
+        tempObj[Object.keys(this.storage)[i]] = this.storage[i];
+      }
+      this.storage = tempObj;
+
+      return lastElement;
+    }
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
+
+var aStack = new Stack();
