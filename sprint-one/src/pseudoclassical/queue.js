72f787953c97172ad2a66d29f4b3d2fc24a52b20
ALau2088 <alau12004@yahoo.com>
2019-07-18 01:38:22 -0700

    complete pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.key = 0;
 };
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.key] = value;
+  this.key += 1;
+  return this.storage[this.key];
+};
+Queue.prototype.dequeue = function() {
+  if (this.key !== 0) {
+    var tempObj = {};
+    // Decrement key
+    // Store first element to be dequeue
+    var firstElement = this.storage[Object.keys(this.storage)[0]];
+    for (var i = 1; i < Object.keys(this.storage).length; i++) {
+      tempObj[Object.keys(this.storage)[i]] = this.storage[i];
+    }
+    this.storage = tempObj;
+    return firstElement;
+  }
+};
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
+var aQueue = new Queue();
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.key = 0;
 };
 
+Stack.prototype.push = function(value) {
+  // Add value
+  this.storage[this.key] = value;
+  // increment key
+  this.key += 1;
+};
 
+Stack.prototype.pop = function() {
+  if (this.key !== 0) {
+    var tempObj = {};
+    // Decrement key
+    this.key -= 1;
+    // Store last element to to popped
+    var lastElement = this.storage[this.key];
+
+    for (var i = 0; i < Object.keys(this.storage).length - 1; i++) {
+      tempObj[Object.keys(this.storage)[i]] = this.storage[i];
+    }
+    this.storage = tempObj;
+
+    return lastElement;
+  }
+};
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
+
+var aStack = new Stack();
