a922486d542a0791aa17a80af69908dc00a65c1a
ALau2088 <alau12004@yahoo.com>
2019-07-21 11:06:13 -0700

    complete sprint-two

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,90 @@
 var BinarySearchTree = function(value) {
+  
+  var bTree = Object.create(bTreeMethods); 
+
+  bTree.value = value;
+
+  bTree.left;
+
+  bTree.right;
+
+  return bTree; 
 };
+
+var bTreeMethods = {};
+
+bTreeMethods.insert = function(value) {
+  var check = function(tree) {
+    if (value < tree.value) {
+      if ( tree.left ) {
+        check(tree.left);
+      } else {
+        tree.left = BinarySearchTree(value);
+      }
+    } else {
+      if ( tree.right) {
+        check(tree.right);
+      } else {
+        tree.right = BinarySearchTree(value);
+      }
+    }
+  };
+  check(this);
+};
+
+bTreeMethods.contains = function(value) {
+  var result = false;
+  var checkVal = function(tree) {
+    if (value === tree.value) {
+      return result = true;
+    } 
+    if (tree.left) {
+      checkVal(tree.left);
+    } 
+    if (tree.right) {
+      checkVal(tree.right);
+    }  
+  };
+  checkVal(this);
+  return result;
+};
+
+bTreeMethods.depthFirstLog = function(cb) {
+  var recur = function(tree) {
+    cb(tree.value);
+    if ( tree.left ) {
+      recur(tree.left);
+    }
+    if ( tree.right ) {
+      recur(tree.right);
+    }
+  };
+  recur(this);
+};
+
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,49 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  
+  
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var storInd = this._storage.get(index);
+  // Check if bucket exists
+  if (storInd === undefined) {
+    this._storage.set(index, [[k, v]]);
+  } else {
+    for (var i = 0; i < storInd.length; i++) {
+      // Check if key exists 
+      if (storInd[i][0] === k) {
+        storInd[i] = [k, v];
+      } else {
+        storInd.push([k,v]);
+      }
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var storInd = this._storage.get(index);
+  if (!storInd[0]) {
+    return undefined;
+  }
+  for (var i = 0; i < storInd.length; i++) {
+    if (storInd[i][0] === k) {
+      return storInd[i][1];
+    }
+  } 
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var storInd = this._storage.get(index);
+  for (var i = 0; i < storInd.length; i++) {
+    if (storInd[i][0] === k) {
+      delete storInd[i];
+    }
+  } 
 };
 
 
